9a5ad4ef7643fab819db5b35e830114c42b379b1
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-09 22:34:50 +1000

    Use hspec for unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = describe "addition" $ do
+
+    it "adds two numbers" $
+        1 + 1 `shouldBe` 2
